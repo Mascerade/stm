67768dcdcbfe8f9d3077d823e8d43acbfdf129a8
BinaryWiz <ultimi450@gmail.com>
2021-08-08 11:23:26 -0400

    added a setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='stm',
+   version='0.1',
+   description='A wrapper around Selenium webdriver that allows for easier management of tabs.',
+   license="MIT",
+   long_description=long_description,
+   author='Jason Acheampong',
+   author_email='jason.acheampong24@gmail.com',
+   url="http://www.github.com/Mascerade/stm",
+   packages=['stm'],
+   install_requires=['selenium', 'urllib3']
+)
